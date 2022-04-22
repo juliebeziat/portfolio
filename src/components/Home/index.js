0bfebc084ccbc62e06a7fb07e1803e2947c60ef4
julie-beZiat <juliebeziat.m@gmail.com>
2022-04-22 19:15:34 +0200

    [Style] homepage, responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,16 @@
 import './home.scss';
 
 const Home = () => {
-    return (
-        <div>Hello World</div>
-    );
+  return (
+    <div className="home">
+      <h1 className="home-title">Bonjour</h1>
+      <p className="home-text">Je suis Julie Beziat, développeuse web junior et je recherche une <span>alternance</span> pour Juin 2022.</p>
+      <div className="home-button">
+        <button type="button" className="home-button-cv">CV</button>
+        <button type="button" className="home-button-contact">Contact</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
+import './style/reset.scss'
 import './style/index.scss'
 import reportWebVitals from './reportWebVitals';
 
