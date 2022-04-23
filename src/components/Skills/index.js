d294ddbf872829136fdc66a960ac94a78afac7f4
julie-beZiat <juliebeziat.m@gmail.com>
2022-04-23 16:13:29 +0200

    [Build] create object to store icons for skill page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,9 +1,20 @@
 import './skills.scss';
+import dataSkills from '../../functions/icons';
 
 const Skills = () => {
-    return (
-        <div>Compétences</div>
-    );
+  return (
+    <div className="skills">
+      <h1 className="skills-title">Compétences</h1>
+      <ul className="skills-list">
+        {dataSkills.map((skill) => (
+            <li className="skills-list-skill" key={skill.icon}>
+                <img src={skill.icon} alt="" className="skills-list-skill-icon" />
+                <p className="skills-list-skill-title">{skill.title}</p>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Skills;
---
+++ src/functions/icons.js
@@ -0,0 +1,66 @@
+import bootstrap from '../assets/icons_skills/bootstrap.svg';
+import css from '../assets/icons_skills/css.svg';
+import github from '../assets/icons_skills/github.svg';
+import html from '../assets/icons_skills/html5.svg';
+import js from '../assets/icons_skills/javascript.svg';
+import linux from '../assets/icons_skills/linux.svg';
+import mysql from '../assets/icons_skills/mysql.svg';
+import php from '../assets/icons_skills/php.svg';
+import react from '../assets/icons_skills/react.svg';
+import redux from '../assets/icons_skills/redux.svg';
+import sass from '../assets/icons_skills/sass.svg';
+import vscode from '../assets/icons_skills/vs-code.svg';
+
+const dataSkills = [
+
+    {
+        icon: html, 
+        title: 'HTML'
+    },
+    {
+        icon: css, 
+        title: 'CSS'
+    },
+    {
+        icon: sass, 
+        title: 'SASS'
+    },
+    {
+        icon: bootstrap, 
+        title: 'BootStrap'
+    },
+    {
+        icon: js, 
+        title: 'JavaScript'
+    },
+    {
+        icon: mysql, 
+        title: 'My SQL'
+    },
+    {
+        icon: php, 
+        title: 'PHP'
+    },
+    {
+        icon: react, 
+        title: 'React JS'
+    },
+    {
+        icon: redux, 
+        title: 'Redux'
+    },
+    {
+        icon: github, 
+        title: 'GitHub'
+    },
+    {
+        icon: vscode, 
+        title: 'VSCode'
+    },
+    {
+        icon: linux, 
+        title: 'Linux Ubuntu'
+    },
+];
+
+export default dataSkills;
