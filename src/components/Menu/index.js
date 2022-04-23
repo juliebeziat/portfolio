443a6fcbf45413a2a775d1eccd3ad0f6893c8b42
julie-beZiat <juliebeziat.m@gmail.com>
2022-04-23 10:33:52 +0200

    [Build] toggle menu burger

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,30 @@
 import { NavLink } from 'react-router-dom';
 import './menu.scss';
+import MenuBurger from '../../assets/menu.svg';
+import { useState } from 'react';
 
 const Menu = () => {
+  const [open, setOpen] = useState(false);
+
+  const  toggleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className='menu'>
-      <nav className='menu-navbar'>
+    <div>
+      <div>
+        <button onClick={toggleMenu} className="menu-burger">
+          <img src={MenuBurger} alt="" />
+        </button>
+      </div>
+      {open && <div className='menu'>
+        <nav className='menu-navbar'>
           <NavLink to='/' className='menu-navbar-nav'>Home</NavLink>
           <NavLink to='/competences' className='menu-navbar-nav'>Compétences</NavLink>
           <NavLink to='/projets' className='menu-navbar-nav'>Projets</NavLink>
           <NavLink to='/contact' className='menu-navbar-nav'>Contact</NavLink>
-      </nav>
+        </nav>
+      </div>}
     </div>
   );
 };
