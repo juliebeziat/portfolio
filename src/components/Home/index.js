f121f2b4035689a7156085c7454181062a4f9f96
julie-beZiat <juliebeziat.m@gmail.com>
2022-04-27 15:05:50 +0200

    [Build] handle opening and closing of ModalAlt and add function to close when user click outside

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,42 @@
+// == Import utils, components
+import { useState } from 'react';
+import ModalAlt from '../Modal/Alternance';
+
 // == Import style
 import './home.scss';
 import Portrait from '../../assets/icons/portrait-julie.png';
-// import ModalAlt from '../Modal/Alternance';
 
-const Home = () => (
-  <div className="app-container">
-    <div className="home" id="home">
-      <h1 className="home-title"><span>&#123;</span> Bonjour <span>&#125;</span></h1>
-      <p className="home-text">Je suis Julie Beziat, développeuse web junior et je recherche une <span className="home-text-arrow">=&gt;</span> <span className="home-text-underline">alternance</span> pour Juin 2022 <span className="home-text-dot">••</span></p>
-      <div className="home-profile">
-        <img src={Portrait} alt="portrait" />
-        <div className="home-profile-content">
-          <p className="home-profile-content-title">Julie Beziat</p>
-          <div className="home-profile-content-links">
-            <a target="_blank" rel="noreferrer" href="https://github.com/juliebeZiat" className="home-profile-content-links-1">github</a>
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/julie-beziat/" className="home-profile-content-links-2">linkedin</a>
+const Home = () => {
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div className="app-container">
+      <div className="home" id="home">
+        <h1 className="home-title"><span>&#123;</span> Bonjour <span>&#125;</span></h1>
+        <p className="home-text">
+          Je suis Julie Beziat, développeuse web junior et je recherche une
+          <span className="home-text-arrow">=&gt;</span>
+          <span
+            className="home-text-underline"
+            onClick={() => setOpenModal(!openModal)}
+          >
+            alternance
+          </span>
+          pour Juin 2022 <span className="home-text-dot">••</span>
+        </p>
+        <div className="home-profile">
+          <img src={Portrait} alt="portrait" />
+          <div className="home-profile-content">
+            <p className="home-profile-content-title">Julie Beziat</p>
+            <div className="home-profile-content-links">
+              <a target="_blank" rel="noreferrer" href="https://github.com/juliebeZiat" className="home-profile-content-links-1">github</a>
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/julie-beziat/" className="home-profile-content-links-2">linkedin</a>
+            </div>
           </div>
         </div>
       </div>
+      {openModal && <ModalAlt />}
     </div>
-    {/* <ModalAlt /> */}
-  </div>
-);
+  );
+};
 
 export default Home;
