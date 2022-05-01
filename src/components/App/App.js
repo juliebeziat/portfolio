011bb5e9cc1dce1260ac324e0cdb533ba3005514
julie-beZiat <juliebeziat.m@gmail.com>
2022-05-01 14:02:52 +0200

    [Build] add darktheme useState and toggle button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 
 // == Import utils
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 // == Import components
 import Menu from '../Menu';
@@ -12,19 +13,24 @@ import Contact from '../Contact';
 import Home from '../Home';
 import Error from '../Error';
 
-const App = () => (
-  <div id="light-mode">
-    <Menu />
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/competences" element={<Skills />} />
-        <Route path="/projets" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  return (
+    <div id={darkTheme ? 'light-mode' : 'dark-mode'}>
+      <Menu />
+      <button className="change-mode" type="button" onClick={() => setDarkTheme(!darkTheme)}>CHANGE MODE</button>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/competences" element={<Skills />} />
+          <Route path="/projets" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
